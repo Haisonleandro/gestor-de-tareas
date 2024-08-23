2bd2da91778134d304df4c9d21865a9a2476948c
Haisonleandro <haisonleandrotoro@gmail.com>
2024-08-23 09:38:54 -0500

    creamos el modelo

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tareas(models.Model):
+    estados = [{'Terminado':'Tarea completa'},
+               {'incompleto':'Tarea pendiente'}
+               ]              
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=500)
+    estado = models.CharField(choices=estados,default='incompleto')
+    def __str__(self):
+        return self.titulo
+
