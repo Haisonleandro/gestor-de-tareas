b967e8db95be5cd9cecd79be558fd55a69a3fcff
Haisonleandro <haisonleandrotoro@gmail.com>
2024-08-23 10:41:12 -0500

    terminamos ya registra las tareas y solucione errores

--- gestion/models.py
+++ gestion/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Tareas(models.Model):
-    estados = [{'terminado':'tarea completa'},
-               {'incompleto':'tarea pendiente'}
-               ]              
+    estados = [('terminado','tarea completa'),
+               ('incompleto','tarea pendiente')]              
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField(max_length=500)
     estado = models.CharField(max_length=100,choices=estados,default='incompleto')
